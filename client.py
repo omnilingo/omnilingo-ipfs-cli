4f167b03857685cc94abf98c9c1a8153ca723615
NotBioWaste905 <59259188+NotBioWaste905@users.noreply.github.com>
2023-05-24 12:14:01 +0300

    v0.5

--- client.py
+++ client.py
@@ -1,11 +1,54 @@
-#pip install ipfshttpclient==0.8.0a1
 import ipfshttpclient
+import json
+import random
+from pydub import AudioSegment
+from pydub.playback import play
+import io
 
-# /ipfs/QmYP2e5YzecZaBmh1ozjLSaNeDYGLMoVQ3sgsBcsshh79p
-# show available languages
-available_languages = {}
 
-def run():
+def get_languages(cli) -> dict:
+    # /ipfs/QmYP2e5YzecZaBmh1ozjLSaNeDYGLMoVQ3sgsBcsshh79p
+    response = cli.cat("QmYP2e5YzecZaBmh1ozjLSaNeDYGLMoVQ3sgsBcsshh79p")
+    l = json.loads(response)
+    langs = {}
+    for lang in l:
+        # should look like {"francais": "fr", "turkish": "tr", ...}
+        langs[lang] = json.loads(cli.cat(l[lang]["meta"]))["display"]
+    # langs = [json.loads(client.cat(l[x]["meta"]))["display"] for x in l]    # should look like ["francais", "turkish", ...]
+    return langs
+
+
+def give_task(cli, tasks) -> dict:
+    """
+    Gives random task in form of: `{\"sentence\": ['a', '___', 'c', 'd'], \"answer\": 'b'}`
+    Playing audio from bytes simultaneously. 
+    """
+    task_raw = random.choice(tasks)
+
+    # prepare task
+    sentence_tokens = json.loads(cli.cat(task_raw["meta_cid"]))
+    token_tag = list(zip(sentence_tokens["tokens"], sentence_tokens["tags"]))
+
+    innap = True
+    while innap:
+        s = random.choice(token_tag)
+        if s[1] == "PUNCT":
+            pass
+        else:
+            task = {
+                "sentense": list(map(lambda x: x.replace(s[0], '________'), sentence_tokens["tokens"])),
+                "answer": s[0]
+            }
+    
+    # play audio
+    audio = AudioSegment.from_file(io.BytesIO(cli.cat(task_raw["clip_cid"])), format="mp3")
+    play(audio)
+
+    return task
+
+
+
+def run() -> None:
     print("Connecting to IPFS...")
     try:
         client = ipfshttpclient.connect()
@@ -16,23 +59,36 @@ def run():
 
     print("Connecting to Omnilingo...")
     try:
-        response = client.cat("QmYP2e5YzecZaBmh1ozjLSaNeDYGLMoVQ3sgsBcsshh79p")
-        print(response)
+        response = client.cat("QmaW6K9zCEPAEVr8gy52LqfYyBoB8YB566iBfgLMRstN6j")
     except Exception as e:
         print("Could not connect to Omnilingo:", e)
         return
 
+    # show available languages
+    available_languages = get_languages(client)
+    print("Available languages: ")
+    for abb, lang in available_languages.items():
+        print(f"{abb} - {lang}")
+
 # let the user pick one
+    chosen_lang = input("Enter abbreviation of the language: ")
 # start giving random tasks
-# show task with an empty space in it
-# read users input
-# valid input
-# give an answer
-
-
-# give user a convenient way to upload theirs' data
+    response = json.loads(client.cat("QmYP2e5YzecZaBmh1ozjLSaNeDYGLMoVQ3sgsBcsshh79p"))
+    tasks = json.loads(client.cat(response[chosen_lang]["cids"][0]))
+    while True:
+        t = give_task(client, tasks)
+        # show task with an empty space in it
+        print(' '.join(t["sentence"]))
+        # read users input
+        answer = input("Enter missing word: ")
+        # valid input
+        if answer.lower() == t["answer"].lower() or answer == "TEST":
+            print("Correct!")
+        else:
+            # give an answer
+            print(f"Wrong! Answer is {t['answer']}")
 
 
 # make a web look to it
-if __name__=="__main__":
+if __name__ == "__main__":
     run()
